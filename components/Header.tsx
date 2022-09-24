75ea02bac3066b702158eb39937584a17dba6931
yuweichen1008 <yuweiichen@gmail.com>
2022-09-24 10:01:46 +0800

    make header bg transparent

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
 
 function Header() {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if(window.scrollY > 0){
+            setIsScrolled(true)
+        }else{
+            setIsScrolled(false)
+        }
+      }
+
+      window.addEventListener("scroll", handleScroll)
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll)
+      }
+    }, [])
+    
     return (
-        <header>
+        <header className={`${isScrolled && 'bg-[#141414]'}`}>
             <div className='flex items-center space-x-2 md:space-x-10'>
                 <img
                     src='https://top10.netflix.com/images/logo.png'
