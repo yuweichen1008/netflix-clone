ed85245a38ff0cc062636384a3892939ed233c30
yuweichen1008 <yuweiichen@gmail.com>
2022-09-24 09:56:18 +0800

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header */}
+      <Header />
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+        </section>
+      </main>
+      {/* Modal */}
+    </div>
+  )
+}
+
+export default Home
